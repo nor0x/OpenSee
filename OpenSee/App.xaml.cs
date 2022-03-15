ecdcf2f81e9b9d9aa96b7d261dd55185d041428e
Johnny <nor0x@users.noreply.github.com>
2022-03-15 20:34:55 +0100

    🪟 size fixed

--- OpenSee/App.xaml.cs
+++ OpenSee/App.xaml.cs
@@ -1,11 +1,27 @@
-﻿namespace OpenSee;
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+
+namespace OpenSee;
 
 public partial class App : Application
 {
+    int width = 1200;
+    int height = 700;
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new MainPage();
+        Microsoft.Maui.Handlers.WindowHandler.WindowMapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+#if WINDOWS
+            var nativeWindow = handler.NativeView;
+            nativeWindow.Activate();
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            //TODO: this is hardcoded stuff for fullhd
+            appWindow.MoveAndResize(new Windows.Graphics.RectInt32((1920 / 2) - width / 2, (1080 / 2) - height / 2, width, height));
+#endif
+        });
+        MainPage = new MainPage();
 	}
 }
