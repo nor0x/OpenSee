2299fc7f651025bc662ced6a5ffc53fcd509f50c
nor0x <nor0x@users.noreply.github.com>
2022-03-21 09:22:40 +0100

    macOS XF project added

---
+++ OpenSeeXF/OpenSeeXF/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OpenSeeXF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void DownloadButton_Clicked(object sender, EventArgs e)
+        {
+        }
+
+        private void QualitySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+        }
+
+        private void SettingsButton_Clicked(object sender, EventArgs e)
+        {
+        }
+
+        private async void OpenDownloadsButton_Clicked(object sender, EventArgs e)
+        {
+        }
+    }
+}
+
