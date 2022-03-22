ea44090cec725e5a684d2d5d433907f5ffecc618
nor0x <nor0x@users.noreply.github.com>
2022-03-22 11:38:09 +0100

    static background 🖼 for macOS

---
+++ OpenSeeXF/OpenSeeXF.MacOS/TiledImageEffectMacOS.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AppKit;
+using OpenSeeXF.MacOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+
+[assembly: ExportEffect(typeof(TiledImageEffect), nameof(TiledImageEffect))]
+namespace OpenSeeXF.MacOS
+{
+    public class TiledImageEffect : PlatformEffect
+    {
+
+
+        protected override void OnAttached()
+        {
+            Console.WriteLine("control: " + Control.GetType().FullName);
+            if(Control is NSImageView imgView)
+            {
+                var c = NSColor.FromPatternImage(imgView.Image);
+                imgView.Image = null;
+                Control.NeedsDisplay = true;
+                Control.WantsLayer = true;
+                Control.Layer.BackgroundColor = c.CGColor;
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
