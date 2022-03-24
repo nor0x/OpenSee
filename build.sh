0b753def7b46f55294fe747f483ae4d019e2bb95
nor0x <nor0x@users.noreply.github.com>
2022-03-24 22:02:24 +0100

    build script 🏗 and codesign stuff 🔑

---
+++ build.sh
@@ -0,0 +1,14 @@
+echo "restoring nugets"
+nuget restore OpenSee.Common/OpenSee.Common.csproj
+nuget restore OpenSeeXF/OpenSeeXF/OpenSeeXF.csproj
+nuget restore OpenSeeXF/OpenSeeXF.MacOS/OpenSeeXF.MacOS.csproj
+
+echo "building class libs"
+dotnet build OpenSeeXF/OpenSeeXF/OpenSeeXF.csproj -c Release
+dotnet build OpenSee.Common/OpenSee.Common.csproj -c Release
+
+
+#Preview Version of VS4Mac
+#/Applications/Visual\ Studio\ \(Preview\).app/Contents/MacOS/vstool build -t:Build -c:"Release" ~/Documents/dev/git/own/OpenSee/OpenSee.sln
+
+/Applications/Visual\ Studio.app/Contents/MacOS/vstool build -t:Build -c:"Release" ~/Documents/dev/git/own/OpenSee/OpenSee.sln
