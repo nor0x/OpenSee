aa1eb128b9c227c51e8c1e540923b75acfc66881
nor0x <nor0x@users.noreply.github.com>
2022-03-25 07:52:50 +0100

    env variables are hard 🙈

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ dotnet build OpenSee.Common/OpenSee.Common.csproj -c Release
 #Preview Version of VS4Mac
 #/Applications/Visual\ Studio\ \(Preview\).app/Contents/MacOS/vstool build -t:Build -c:"Release" ~/Documents/dev/git/own/OpenSee/OpenSee.sln
 
-/Applications/Visual\ Studio.app/Contents/MacOS/vstool build -t:Build -c:"Release" {$GITHUB_WORKSPACE}/OpenSee.sln
+/Applications/Visual\ Studio.app/Contents/MacOS/vstool build -t:Build -c:"Release" $GITHUB_WORKSPACE/OpenSee.sln
 
 cd $GITHUB_WORKSPACE/OpenSeeXF/OpenSeeXF.MacOS/bin/Release
 ditto -c -k --sequesterRsrc --keepParent OpenSee.app OpenSee.app.zip 
