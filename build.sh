76e1e389782f909f26f516356dd3e7673838e383
nor0x <nor0x@users.noreply.github.com>
2022-04-14 11:11:37 +0200

    build script updated 🏗

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
-echo "restoring nugets"
+find . -iname "bin" | xargs rm -rf
+find . -iname "obj" | xargs rm -rf
+
+
+# echo "restoring nugets"
 nuget restore OpenSee.Common/OpenSee.Common.csproj
 nuget restore OpenSeeXF/OpenSeeXF/OpenSeeXF.csproj
 nuget restore OpenSeeXF/OpenSeeXF.MacOS/OpenSeeXF.MacOS.csproj
 
-echo "building class libs"
+# echo "building class libs"
 dotnet build OpenSeeXF/OpenSeeXF/OpenSeeXF.csproj -c Release
 dotnet build OpenSee.Common/OpenSee.Common.csproj -c Release
 
-
-#Preview Version of VS4Mac
-#/Applications/Visual\ Studio\ \(Preview\).app/Contents/MacOS/vstool build -t:Build -c:"Release" ~/Documents/dev/git/own/OpenSee/OpenSee.sln
-
-/Applications/Visual\ Studio.app/Contents/MacOS/vstool build -t:Build -c:"Release" $GITHUB_WORKSPACE/OpenSee.sln
+msbuild OpenSee.sln /target:OpenSeeXF_MacOS:Rebuild /p:Configuration=Release
 
 cd $GITHUB_WORKSPACE/OpenSeeXF/OpenSeeXF.MacOS/bin/Release
+
 ditto -c -k --sequesterRsrc --keepParent OpenSee.app OpenSee.app.zip 
